21fb811f225b69aa5e062dce05e2f8445ccaf50a
domac <domacode@gmail.com>
2018-11-17 16:26:33 +0800

    add threading

---
+++ cpp/ch04/mtsleep.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import threading
+
+from time import ctime, sleep
+
+loops = [2, 4]
+
+
+class ThreadFunc(object):
+    def __init__(self, func, args, name=''):
+        self.name = name
+        self.args = args
+        self.func = func
+
+    def __call__(self):
+        self.func(*self.args)
+
+
+def loop(nloop, nsec):
+    print 'starting at:', ctime()
+    sleep(nsec)
+    print 'loop', nloop, 'done at', ctime()
+
+
+def main():
+    print 'starting at:', ctime()
+    threads = []
+    nloops = range(len(loops))
+
+    for i in nloops:
+        t = threading.Thread(
+            target=ThreadFunc(loop, (i, loops[i]), loop.__name__))
+
+        threads.append(t)
+
+    for i in nloops:
+        threads[i].start()
+
+    for i in nloops:
+        threads[i].join()
+
+    print 'all DONE at:', ctime()
+
+
+if __name__ == '__main__':
+    main()
