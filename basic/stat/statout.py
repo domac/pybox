90f2a70ab227d0a7ed71c3e3f8c8213fec789eb7
domac <domacode@gmail.com>
2018-12-01 19:00:48 +0800

    update tool

--- basic/stat/statout.py
+++ basic/stat/statout.py
@@ -7,6 +7,8 @@ import sys
 
 cmd = ''
 op = ''
+start = ''
+end = ''
 regex = '(\d{4}[-/]\d{2}[-/]\d{2}\s+\d{2}\:\d{2}\:\d{2})\.\d+\s+\[.\]\s+\[main.\(\*ConnscStat\).PrintStats\] \[stat.go.\d+\]\s+>>>\s+cmd\s+\[(\d+)\]\s+(\w+)(\s+\w+)+:\s?(\d+)'
 
 reg = re.compile(regex)
@@ -19,8 +21,13 @@ def output(line):
         groups = match.groups()
         #print groups
         if (cmd == '' or cmd == groups[1]) and (op == '' or op == groups[2]):
-            print '%s %d %s %d' % (groups[0], int(groups[1]), groups[2],
-                                   int(groups[4]))
+
+            datetime = groups[0]
+
+            if datetime >= start and datetime <= end:
+
+                print '%s %d %s %d' % (groups[0], int(groups[1]), groups[2],
+                                       int(groups[4]))
 
 
 def readlines(filename):
@@ -34,6 +41,10 @@ if __name__ == "__main__":
     parser.add_argument('-f', type=str, help='filepath of stat log')
     parser.add_argument('-cmd', type=str, default='', help='cmd')
     parser.add_argument('-op', type=str, default='', help='operator')
+
+    parser.add_argument(
+        '-start', type=str, default='2018', help='start datetime')
+    parser.add_argument('-end', type=str, default='2999', help='end datetime')
     args = parser.parse_args()
     print 'query cmd: %s, op: %s' % (args.cmd, args.op)
 
@@ -43,4 +54,6 @@ if __name__ == "__main__":
 
     cmd = args.cmd
     op = args.op
+    start = args.start
+    end = args.end
     readlines(args.f)
